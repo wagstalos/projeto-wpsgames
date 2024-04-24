bbab8f03822e6a4c6281825cfd81f57fc704fd3d
Wagner Paulo <wagstalos@hotmail.com>
2024-04-24 12:42:25 -0300

    added : slider projects

--- js/scripts/slider.js
+++ js/scripts/slider.js
@@ -1,29 +1,10 @@
-function SliderCustomersTestimonial() {
-  var swiper4 = new Swiper(".mySwiper2", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      400: {
-        slidesPerView: 1,
-      },
-      800: {
-        slidesPerView: 3,
-      },
-    },
-  });
-}
-
-SliderCustomersTestimonial();
-
 var swiper2 = new Swiper(".mySwiper", {
-  spaceBetween: 20,
+  spaceBetween: 10,
   freeMode: false,
   slidesPerView: 1.1,
+
+  loop: true,
+  navigation: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -39,10 +20,6 @@ var swiper2 = new Swiper(".mySwiper", {
       spaceBetween: 10,
     },
     900: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1200: {
       slidesPerView: 3,
       spaceBetween: 20,
     },
