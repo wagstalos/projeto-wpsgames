56a2e381af6979c50842d04145f6eaa8817cb391
Wagner Paulo <wagstalos@hotmail.com>
2024-04-26 21:00:22 -0300

    📦 NEW: menu

--- js/scripts/buttons.js
+++ js/scripts/buttons.js
@@ -1,24 +1,48 @@
+// function menuMobile() {
+//   var dotsButton = document.querySelector(".dots");
+//   var menuMobile = document.querySelector(".menu-mobile");
+//   var closeIcon = document.querySelector(".icon-tabler-x");
+//   var links = document.querySelectorAll(".menu-mobile__links a");
+
+//   dotsButton.addEventListener("click", function () {
+//     menuMobile.style.display = "block";
+//     closeIcon.style.display = "block";
+//   });
+
+//   closeIcon.addEventListener("click", function () {
+//     menuMobile.style.display = "none";
+//     closeIcon.style.display = "none";
+//   });
+
+//   links.forEach(function (link) {
+//     link.addEventListener("click", function () {
+//       menuMobile.style.display = "none";
+//       closeIcon.style.display = "none";
+//     });
+//   });
+// }
+
+// menuMobile();
+
 function menuMobile() {
-  var dotsButton = document.querySelector(".dots");
-  var menuMobile = document.querySelector(".menu-mobile");
-  var closeIcon = document.querySelector(".icon-tabler-x");
-  var links = document.querySelectorAll(".menu-mobile__links a");
+  var dotsButton = $(".dots");
+  var menuMobile = $(".menu-mobile");
+  var closeIcon = $(".icon-tabler-x");
+  var links = $(".menu-mobile__links a");
 
-  dotsButton.addEventListener("click", function () {
-    menuMobile.style.display = "block";
-    closeIcon.style.display = "block";
+  dotsButton.on("click", function () {
+    menuMobile.slideDown();
+    closeIcon.css("display", "block");
   });
 
-  closeIcon.addEventListener("click", function () {
-    menuMobile.style.display = "none";
-    closeIcon.style.display = "none";
+  closeIcon.on("click", function () {
+    menuMobile.slideUp();
+    closeIcon.css("display", "none");
   });
 
-  links.forEach(function (link) {
-    link.addEventListener("click", function () {
-      menuMobile.style.display = "none";
-      closeIcon.style.display = "none";
-    });
+  links.on("click", function () {
+    menuMobile.slideUp();
+    closeIcon.css("display", "none");
   });
 }
 
