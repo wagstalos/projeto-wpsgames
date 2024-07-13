f4acd1e41159e0b5c77f7dbc171e0bda5f3fcc6b
Wagner Paulo <wagstalos@hotmail.com>
2024-07-13 12:08:21 -0300

    ajuste slider

---
+++ js/scripts/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  var loader = document.getElementById("loader");
+  var content = document.querySelector(".content");
+
+  loader.style.display = "none";
+  content.style.display = "block";
+});
